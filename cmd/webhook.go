4f75c4d876680af169e6227db50c6920cf8da5bb
Jose Matsuda <jose.matsuda@statcan.gc.ca>
2024-10-17 13:53:52 -0400

    try infinitely

--- cmd/webhook.go
+++ cmd/webhook.go
@@ -287,9 +287,9 @@ func createPatch(pod *corev1.Pod, sidecarConfigTemplate *Config, annotations map
 			filerBucketName := limitString(svmName, 5) + "-" + hashedBucketName
 			// Configure the sidecar container
 			sidecarConfig.Containers[0].Name = filerBucketName
-			sidecarConfig.Containers[0].Args = []string{"-c", "/goofys --cheap --endpoint " + s3Url +
+			sidecarConfig.Containers[0].Args = []string{"-c", "while :; do /goofys --cheap --endpoint " + s3Url +
 				" --http-timeout 1500s --dir-mode 0777 --file-mode 0777  --debug_fuse --debug_s3 -o allow_other -f " +
-				hashedBucketName + "/ /tmp; echo sleeping...; sleep infinity"}
+				hashedBucketName + "/ /tmp; sleep 1; done"}
 
 			sidecarConfig.Containers[0].Env[0].Value = "fusermount3-proxy-" + filerBucketName + "-" + pod.Namespace + "/fuse-csi-ephemeral.sock"
 			sidecarConfig.Containers[0].Env[1].Value = s3Access
