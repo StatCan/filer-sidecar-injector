1cf3385f39ac6fd1e45c09bf81da86d45dce0fea
Jose Manuel (Ito) <jose.matsuda@statcan.gc.ca>
2025-03-04 08:58:26 -0500

    chore(cpu): update resource (#25)
    * chore(requests+limits): address throttling

--- cmd/webhook.go
+++ cmd/webhook.go
@@ -242,11 +242,11 @@ func createPatch(pod *corev1.Pod, sidecarConfigTemplate *Config, annotations map
 	svmShareList *corev1.ConfigMap, svmInfoMap map[string]SvmInfo) ([]byte, error) {
 	var patch []patchOperation
 	resourceRequest := map[corev1.ResourceName]resource.Quantity{
-		"cpu":    resource.MustParse("0.003"),
+		"cpu":    resource.MustParse("0.1"),
 		"memory": resource.MustParse("100Mi"),
 	}
 	resourceLimit := map[corev1.ResourceName]resource.Quantity{
-		"cpu":    resource.MustParse("0.25"),
+		"cpu":    resource.MustParse("0.5"),
 		"memory": resource.MustParse("10Gi"),
 	}
 	isFirstVol := true
